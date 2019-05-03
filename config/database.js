485eec6243d9c1c342eaf9b0a0b3a2d210a97590
Apple <apple@Apples-MacBook-Air.local>
2019-05-03 15:43:55 +0530

    updated db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
     mongoURI:
-      'mongodb://singhkumarhemant:Agile@1122@ds149596.mlab.com:49596/expressadminpanel'
+      'mongodb://expressadminpanel:Agile1122@ds149596.mlab.com:49596/expressadminpanel'
   };
 } else {
   module.exports = { mongoURI: 'mongodb://localhost/expressAdminPanel' };
