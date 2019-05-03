fdabcd327e72bab081258d56026bb061e4328f62
Hemant Kumar Singh <42830161+singhkumarhemant@users.noreply.github.com>
2019-05-03 14:57:20 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://hemant:hemant1234@ds251022.mlab.com:51022/expressAdminPanel'}
+    module.exports = {mongoURI: 'mongodb://hemant:hemant1234@ds251022.mlab.com:51022/expressadminpanel'}
   } else {
     module.exports = {mongoURI: 'mongodb://localhost/expressAdminPanel'}
   }
